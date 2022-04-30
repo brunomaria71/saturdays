fa39a4740b39f749476ca0d60a8f7db197abcfd5
brunomaria71 <brunomaria71@yahoo.com>
2022-04-30 13:51:58 -0400

    last minute save

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-import { initializeApp, cert } from "firebase-admin/app";
+import { initializeApp, cert, getApps} from "firebase-admin/app";
+import { getFirestore } from "firebase-admin/firestore"
 
 import serviceAccount from "./credentials.js";
 
-var admin = require("firebase-admin");
+const getDb = () => {
+    if (getApps.length === 0){
+        initializeApp({
+            credential: cert(serviceAccount)
+        })
+    }
+    return getFirestore();
+}
 
-var serviceAccount = require("path/to/serviceAccountKey.json");
+const getAttendees = async () => {
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+    const db = getDb()
+// get the collection
+    const attendeeCollection = db.collection('attendees')
+// get all the docs of the collection
+try {
+    const snapshot = await attendeeCollection.get()
+    const attendees = []
+    snapshot.docs.forEach(doc => {
+        attendees.push({id: doc.id, ...doc.data()}) // ...doc will gather not just the id, but also the data 
+    })
+
+    return attendees;
+
+} catch (error) {
+    console.error(eror)
+}
+
+
